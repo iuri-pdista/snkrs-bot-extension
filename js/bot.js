ef0f5526751390ef4d84eb13a45bd8d1d36ccd07
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-11 16:10:23 -0300

    finish purchase

--- js/bot.js
+++ js/bot.js
@@ -1,4 +1,18 @@
-function submit(){
-    console.log("clickou")
+document.getElementById('btn-comprar')?.click();
+if(location.href === "https://www.nike.com.br/Carrinho"){ 
+    location.assign("https://www.nike.com.br/Checkout");
 }
-submit();
+if(location.href === "https://www.nike.com.br/Checkout"){
+    document.getElementById("seguir-pagamento")?.addEventListener("click", event => event.preventDefault())
+    document.getElementById("seguir-pagamento")?.click();
+    const code = document.getElementsByClassName("ModalCorpoCentralizado ")[0]?.id.split("_")[1];
+    document.getElementsByName("btn0_modalNotice_"+code)[0]?.click();
+    document.getElementsByClassName("close")[1]?.click();
+    const [arrow] = document.getElementsByClassName("select-cta-arrow")
+    arrow?.click()
+    document.getElementsByName("ccidradio")[1]?.click()
+    document.getElementById("politica-trocas")?.click()
+}
+
+
+
